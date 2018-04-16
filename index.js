16136ec34d8406efe77bff03584385e1ee02bb2f
Steve Suranant <053steve@gmail.com>
2018-04-16 23:24:22 +0700

    [CODE] Integrated react native navigator

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('reactnativetemplate', () => App);
+import {Navigation} from 'react-native-navigation';
+import App from './src/App';
+
+
+Navigation.registerComponent('reactnativetemplate', () => App);
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: 'reactnativetemplate',
+    title: 'Welcome'
+  }
+});
+
+
